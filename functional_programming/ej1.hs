f99104fe5997dc78df788513e89dcf424ae6b27b
Joaquin Ignacio Romero <joaquinromeroignacio@gmail.com>
2025-10-02 19:59:11 -0300

    comi

--- functional_programming/ej1.hs
+++ functional_programming/ej1.hs
@@ -1,5 +1,4 @@
 import Data.Text.IO (hPutStrLn)
-import Prelude hiding (gcd)
 main :: IO()
 main = putStrLn "HOlat mundo"
-gcd a b = if b == 0 then a else gcd b (a `mod` b)
+gcd a b = if b == 0 then a else gcd b (a b)
