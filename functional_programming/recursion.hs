b1caa673bdebf73c3c1ad5c8be4876afd6380674
Joaquin Ignacio Romero <joaquinromeroignacio@gmail.com>
2025-10-03 22:06:41 -0300

    comit clase viernes

--- functional_programming/recursion.hs
+++ functional_programming/recursion.hs
@@ -1,1 +1,8 @@
 --RECURSION GUARDS PATTERNS--
+CmpChar:: char -> char -> Bool
+CmpChar c1 c2 = c1 == c2
+
+palindromo:: String -> Bool
+palindromo [] = True
+palindromo [x] = True
+palindromo (x:xs) = tail xs == x && palindromo 
